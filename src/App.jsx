5075d9bb36a80ff13d51e6a23ba415fad45c73ca
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-08-23 00:07:07 +0600

    done todo list

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,19 @@ function getInput(event){
   return (
     <div>
       <Navbar></Navbar>
-      <div className='bg-lime-200 h-screen flex flex-col items-center '>
-        <div className='bg-amber-400'>
+      <div className='bg-lime-200 h-screen flex flex-col items-center  w-1/2 mx-auto'>
+        <div className='bg-green-400 p-4'>
           <input value={initial} onChange={getInput} className='p-2 border-black border-1 rounded-l-lg focus:outline' type="text" placeholder='Type here' />
-          <button onClick={getData} className='bg-blue-600 p-4 m-4 rounded-xl text-white'>Add</button>
+          <button onClick={getData} className='bg-black p-4 m-4 rounded-xl text-white'>Add</button>
         </div>
 
-        <div className='bg-white '>
-          List Of Tasks
-          {
-            data.map((da, index) => <List key={index} da={da} deleteTask={deleteTask}  index={index}></List>)
+        <div className='bg-white flex flex-col items-center justify-center w-1/2 mx-auto py-4'>
+        <h2 className='text-xl font-bold mb-4'>List Of Tasks</h2>
+          <div className='w-full px-4'>
+          {data.length === 0 ? (<p>No Task yet!</p>) : (data.map((da, index) => <List key={index} da={da} deleteTask={deleteTask}  index={index}></List>))
+            
           }
+          </div>
         </div>
       </div>
     </div>
--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { MdDeleteOutline } from "react-icons/md";
 
 const List = ({da, index, deleteTask}) => {
 
     return (
-        <div>
-            <h1>{da}</h1>
-<button onClick={()=>deleteTask(index)}>delete</button>
+        <div className='flex justify-between gap-6'>
+            <h1 className='ml-4'>{da}</h1>
+<button onClick={()=>deleteTask(index)}><MdDeleteOutline className='text-3xl mr-4'></MdDeleteOutline></button>
         </div>
     );
 };
